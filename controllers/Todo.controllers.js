b5426e5e4c463242a602cae057d6022e110a23af
Prathamesh61 <rawoolprathamesh61@gmail.com>
2022-11-07 02:31:43 +0530

    first

--- controllers/Todo.controllers.js
+++ controllers/Todo.controllers.js
@@ -42,7 +42,7 @@ const patchTodos = async (req, res) => {
 const deleteTodos = async (req, res) => {
     let { user_id } = req.body;
     let { id } = req.params;
-    console.log(_id, user_id);
+    console.log(id, user_id);
     const data = await Todo.findOneAndDelete({ user_id, _id:id });
     await data;
     let remaining = await Todo.find({ user_id });
