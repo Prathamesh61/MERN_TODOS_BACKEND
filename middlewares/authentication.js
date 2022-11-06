14e1071ac5745bb9c8b48e9c016286c1693ddd58
Prathamesh61 <rawoolprathamesh61@gmail.com>
2022-11-07 00:30:44 +0530

    first

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,19 +3,19 @@ require('dotenv').config();
 const authentication = (req, res, next) => {
     const token = req.headers?.authorization?.split(" ")[1];
     if (!token) {
-      res.send({ msg: "Login Again" });
+        res.send({ msg: "Login Again" });
     } else {
-      const decoded = jwt.verify(token, "abcd1234");
-      const { user_id } = decoded;
-    
-      if (decoded) {
-        req.body.user_id = user_id;
-        next();
-      } else {
-        res.send({ msg: "Please Login Again" });
-      }
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const { user_id } = decoded;
+
+        if (decoded) {
+            req.body.user_id = user_id;
+            next();
+        } else {
+            res.send({ msg: "Please Login Again" });
+        }
     }
-  };
+};
 module.exports = {
     authentication
 }
